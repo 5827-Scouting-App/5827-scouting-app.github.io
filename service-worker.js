f8994520951bce5b409540bcd0f89681c823dd92
Anirudh <anirudh.suresh@outlook.com>
2019-10-09 20:01:02 -0700

    service worker -- should be pwa

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+console.log("[STARTUP] Service worker init");
+
+var cacheName = 'hello-world-page';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/res/bulma.min.css',
+  '/res/instascan.min.js',
+  '/res/jquery.min.js',
+  '/res/qr-scanner-worker.min.js',
+  '/res/qr-scanner.min.js',
+  '/res/qrcode.js',
+  '/export.html',
+  '/import.html',
+  '/service-worker.js'
+];
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
